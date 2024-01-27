524b3946f03e80341dc1e687ba45425a128dd016
Ritul <rituls619@gmail.com>
2024-01-27 19:22:21 +0100

    ~

--- main.py
+++ main.py
@@ -4,11 +4,22 @@ import random
 # input => experiment, database, conc, file sizes
 
 # output => relevant vizs, logs
+def ingest_inputs(): # take in config metrics from the yam file
+    pass
+
 
 def generate_rand_bytes(size):
     return bytes(random.choices(range(256), k=size))
 
+def exp_lib():
+    pass
 
+
+def run_exps(): # to be run by the driver
+    # size, exps to run, concurrency limit
+    # invoke lambdas
+    # 
+    pass
 
 # Example usage:
 size_in_bytes = 100
---
+++ tests/exps.py
@@ -0,0 +1,17 @@
+
+# single write
+
+# single read
+
+# single delete
+
+# single update
+
+# sequential write
+
+# sequential read
+
+# random write
+
+# random read
+
